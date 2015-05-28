ffae5e43c8f02df99bea9430aeb17b337e100ca1
Matthew Francis Brunetti <zenflow87@gmail.com>
2015-05-28 18:50:19 -0400

    Don't throw error fetching 0 emails

--- test/index.js
+++ test/index.js
@@ -5,5 +5,5 @@ var imap_query = new ImapQuery(account, [["SUBJECT", "message from SPOT"], ["ALL
 
 imap_query.check(function(error, updated){
     if (error){throw error;}
-    console.log('found '+imap_query.messages().length+' messages');
+    console.log('returned '+imap_query.messages().length+' messages');
 });
